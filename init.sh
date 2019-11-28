2f1845f6ec8e034fa2bce63d743225a7045e77f2
Elyar <1595685558@qq.com>
2019-11-28 21:09:33 +0800

    fix docker

--- init.sh
+++ init.sh
@@ -1,2 +1,2 @@
-docker cp ./mongodb/prod_data name-mongo:/data
+# docker cp ./mongodb/prod_data name-mongo:/data
 docker exec name-mongo mongorestore -h 127.0.0.1:27017 -d name-tinder ./data/prod_data/name-tinder 
